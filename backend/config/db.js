ad9175a8d74d8011580db7b37c95a5d37ddf6e1c
Osamaabdul <ibrahimabdulosama@gmail.com>
2025-08-14 23:56:11 +0100

    changed URI to URL

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 const connectToDb = async() => {
    try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("Connected to the database successfully");
    } catch (error) {
     console.error("Error connecting to the database:", error);
